d6064143d39734a2d80a1b2bd4076186beaba587
Tilman Potthof <tilmanpotthof@googlemail.com>
2017-09-12 22:19:19 +0200

    basic module format in apply.json
    - list of version with commands and check commands, to verify if commands were applied
    - example check commands for typescript

--- src/dev-tools-apply.js
+++ src/dev-tools-apply.js
@@ -1,6 +1,53 @@
 
+const fs = require("fs");
+const _ = require("lodash");
+
+const execSync = require("child_process").execSync;
+
+const modulesPath = __dirname + "/modules/";
+
 module.exports = {
 	run: function () {
 		console.log("dev-tool-apply");
+
+		this.check();
+	},
+	check: function (moduleName) {
+		const modules = this.loadDefinitions();
+
+		if (!moduleName) {
+			Object.keys(modules).forEach(this.check.bind(this));
+			return;
+		}
+
+		console.log(`\n  .:: ${moduleName} ::.`);
+
+		const moduleDefinition = modules[moduleName];
+
+		const moduleVersions = moduleDefinition.versions;
+
+		moduleVersions.forEach(function (moduleVersion) {
+			console.log(`\n  check version ${moduleVersion.version} - ${moduleVersion.description}`);
+			_.forEach(moduleVersion.check, (command, checkDescription) => {
+				try {
+					execSync(command);
+					console.log(`✓ check successful - ${checkDescription} (${moduleName})`);
+				} catch(e) {
+					console.log(`✗ check failed - ${checkDescription} (${moduleName})`);
+				}
+			});
+		});
+
+	},
+	loadDefinitions: function() {
+		const modules = {};
+
+		fs.readdirSync(modulesPath).forEach((moduleName) => {
+			const applyJson = require(modulesPath + moduleName + "/apply.json");
+			modules[moduleName] = applyJson;
+		});
+
+
+		return modules;
 	}
 };
