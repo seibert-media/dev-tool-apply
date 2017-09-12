5b63e896a98db09f4d781eb01ad94e128cd2e033
Tilman Potthof <tilmanpotthof@googlemail.com>
2017-09-13 00:16:29 +0200

    Replace readline with readline-sync

--- src/utils/confirm.js
+++ src/utils/confirm.js
@@ -1,10 +1,6 @@
-const readline = require('readline');
+const readlineSync = require('readline-sync');
 
 function confirm(question, action) {
-	const readlineInterface = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	});
 
 	const inputHint = "[y/n]";
 	const allowedValues = "yYnN".split("");
@@ -13,16 +9,14 @@ function confirm(question, action) {
 	const questionText = `${question} ${inputHint} `;
 
 	function askForConfirmation() {
-		readlineInterface.question(questionText, (answer) => {
-			if (allowedValues.indexOf(answer) < 0) {
-				askForConfirmation();
-			} else {
-				readlineInterface.close();
-				if (confirmingValues.indexOf(answer) >= 0) {
-					action()
-				}
+		const answer = readlineSync.question(questionText);
+		if (allowedValues.indexOf(answer) < 0) {
+			askForConfirmation();
+		} else {
+			if (confirmingValues.indexOf(answer) >= 0) {
+				action()
 			}
-		});
+		}
 	}
 
 	askForConfirmation();
