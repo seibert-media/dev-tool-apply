161fcc02a43bde2d48de934884e537ca625e66c4
Tilman Potthof <tilmanpotthof@googlemail.com>
2017-09-14 09:52:55 +0200

    NpmInstallStrategy: install with --save-dev by default

--- src/utils/command-strategies/NpmInstallStrategy.js
+++ src/utils/command-strategies/NpmInstallStrategy.js
@@ -7,12 +7,14 @@ module.exports = class NpmInstallStrategy {
 	constructor(applyStep) {
 		preconditions.shouldBeDefined(applyStep["npm-module"]).test();
 		this.npmModule = applyStep["npm-module"];
+
+		this.options = applyStep.dev === false ? '--save' : '--save-dev'
 	}
 	check() {
 		return runCommand(`npm ls ${this.npmModule}`).status === 0;
 	}
 	apply() {
-		return runCommand(`npm install --save ${this.npmModule}`, 'log').status === 0;
+		return runCommand(`npm install ${this.options} ${this.npmModule}`, 'log').status === 0;
 	}
 	changedFiles() {
 		return "package.json";
